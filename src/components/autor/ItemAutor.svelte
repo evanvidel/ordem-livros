b022ca3b85ee8e19fd6914557fc46f0e7075dfc7
Evangelista Franco <evanvidel@gmail.com>
2025-03-07 22:35:06 -0300

    creating ItemAutor component

---
+++ src/components/autor/ItemAutor.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let autor: any = null;
+</script>
+
+{#if autor}
+	<a
+		href="/autores/{autor.id}"
+		class="flex flex-col rounded-md relative overflow-hidden boder
+         border-zinc-800"
+	>
+		<img src={autor.imagem} alt="Foto do autor" class="h-96 object-cover" />
+		<div class="absolute bottom-0 flex justify-end px-6 py-4
+         bg-black/50 w-full">
+			<span class="text-2xl font-black">{autor.nome}</span>
+		</div>
+	</a>
+{/if}
--- src/routes/autores/+page.svelte
+++ src/routes/autores/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Autores</h1>
-<a href="/autores/george-martin">George Martin</a>
+<script>
+	import ItemAutor from '../../components/autor/ItemAutor.svelte';
+	import autores from '../../constants/autores';
+</script>
+
+<div class="grid grid-cols-2 gap-5">
+	{#each autores as autor}
+		<ItemAutor {autor} />
+	{/each}
+</div>

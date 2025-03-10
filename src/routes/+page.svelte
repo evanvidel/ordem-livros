52ce7fcdbc78dc630e66411ef71c882a9482c609
Evangelista Franco <evanvidel@gmail.com>
2025-03-09 21:57:50 -0300

    creating the page series

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1 class="text-3xl font-bold">Ordem dos livros</h1>
-<a href="/autores">Autores</a>
+<script>
+	import ItemSerie from '../components/seie/ItemSerie.svelte';
+
+	import series from '../constants/series';
+</script>
+
+<div class="flex flex-col gap-5">
+	{#each series as serie}
+		<ItemSerie {serie} />
+	{/each}
+</div>
